a2f3845941507efb24ea51b2473871a6b9c57e9d
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-22 11:04:18 -0500

    search

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
-export class SearchComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class SearchComponent {
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>;
+  search() {
+    const valir = this.txtSearch.nativeElement.value;
+    console.log(valir);
+    this.txtSearch.nativeElement.value = '';
+  }
 }
