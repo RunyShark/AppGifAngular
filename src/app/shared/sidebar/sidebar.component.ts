3f74a87e2b52c0c6e69b18692f96488e9f59da56
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-22 12:42:21 -0500

    final

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { GifsServiceService } from '../../gifs/services/gifs-service.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-  constructor() {}
+  get historial() {
+    return this.gifsService.historial;
+  }
+
+  constructor(private gifsService: GifsServiceService) {}
+  search(event: string) {
+    console.log(event);
+    this.gifsService.buscarGifHistoria(event);
+  }
 }
