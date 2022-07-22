e66d76747dcbff07d51f66a527d232e5607e9faa
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-22 10:55:36 -0500

    search

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
-  declarations: [GifsPageComponent],
+  declarations: [GifsPageComponent, SearchComponent, ResultsComponent],
   exports: [GifsPageComponent],
   imports: [CommonModule],
 })
